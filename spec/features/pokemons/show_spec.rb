1cffaf6993e3bf179e783a5041cb27e1e474f4a5
Jim Riddle <jimriddle0@gmail.com>
2022-05-11 20:37:51 -0500

    wrote test for user story 4

--- spec/features/pokemons/show_spec.rb
+++ spec/features/pokemons/show_spec.rb
@@ -4,25 +4,16 @@ RSpec.describe 'the pokemons show page' do
   it 'displays the attrs of a specific pokemon' do
     # For each parent table
     # As a visitor
-    # When I visit '/parents'
-    # Then I see the name of each parent record in the system
+    # When I visit ''/child_table_name/:id'
+    # Then I see the child with that id including the child's attributes
     trainer = Trainer.create!(name: "Ash", age: 18, all_8_badges: false)
+    pokemon = trainer.pokemons.create!(name: "Squirtle", level: 12, fainted: false)
+    # pokemon_2 = trainer.pokemons.create!(name: "Charmander", level: 6, fainted: false)
     # trainer_2 = Trainer.create!(name: "Brock", age: 21, all_8_badges: false)
-    visit "/trainers/#{trainer.id}"
+    visit "/pokemons/#{pokemon.id}"
     # save_and_open_page
-    expect(page).to have_content(trainer.name)
-    expect(page).to have_content(trainer.age)
-    expect(page).to have_content(trainer.all_8_badges)
-    # expect(page).to have_content(trainer_2.name)
+    expect(page).to have_content(pokemon.name)
+    expect(page).to have_content(pokemon.level)
+    expect(page).to have_content(pokemon.fainted)
   end
-
-  # it 'displays the name of the artist for the song' do
-  #
-  #   pokemon = trainer.pokemons.create!(name: "Squirtle", level: 12, fainted: false)
-  #   binding.pry
-  #   # pokemon_2 = trainers.pokemons.create!(name: "Charmander", level: 6, fainted: false)
-  #   visit "/trainers/#{trainer.id}"
-  #   expect(page).to have_content(pokemon.name)
-  #   expect(page).to_not have_content(pokemon_2.title)
-  # end
 end
