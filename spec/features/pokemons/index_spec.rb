02f6cc30bd8eca59e2b5b6a5dcf46cb4bf2fa8dc
Jim Riddle <jimriddle0@gmail.com>
2022-05-11 19:37:27 -0500

    wrote test for user_story_3, passes

--- spec/features/pokemons/index_spec.rb
+++ spec/features/pokemons/index_spec.rb
@@ -8,13 +8,15 @@ RSpec.describe 'the pokemons index page' do
     trainer = Trainer.create!(name: "Ash", age: 18, all_8_badges: false)
     pokemon = trainer.pokemons.create!(name: "Squirtle", level: 12, fainted: false)
     pokemon_2 = trainer.pokemons.create!(name: "Charmander", level: 6, fainted: false)
-    binding.pry
-    # pokemon_2 = trainer.pokemons.create!(name: "Charmander", level: 6, fainted: false)
-
-    # visit "/pokemons"
+    # binding.pry
+    visit "/pokemons"
     # save_and_open_page
-    # expect(page).to have_content(pokemon.name)
-    # expect(page).to have_content(pokremon_2.name)
+    expect(page).to have_content(pokemon.name)
+    expect(page).to have_content(pokemon.level)
+    expect(page).to have_content(pokemon.fainted)
+    expect(page).to have_content(pokemon_2.name)
+    expect(page).to have_content(pokemon_2.level)
+    expect(page).to have_content(pokemon_2.fainted)
   end
 
 end
