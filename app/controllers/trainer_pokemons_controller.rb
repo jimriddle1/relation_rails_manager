90979e057f34364b180687a046065c977f4207f3
Jim Riddle <jimriddle0@gmail.com>
2022-05-17 13:07:38 -0500

    modified trainer pokemons controller to include class methods

--- app/controllers/trainer_pokemons_controller.rb
+++ app/controllers/trainer_pokemons_controller.rb
@@ -2,7 +2,7 @@ class TrainerPokemonsController < ApplicationController
   def index
     if params[:column].nil? == false
       @trainer = Trainer.find(params[:trainer_id])
-      @pokemons = @trainer.pokemons.where("pokedex_num >= #{params[:number]}")
+      @pokemons = @trainer.pokemons.above_pokedex_num_threshold(params[:number])
       # binding.pry
     elsif params[:sorted] == "alphabetical"
       @trainer = Trainer.find(params[:trainer_id])
@@ -11,9 +11,7 @@ class TrainerPokemonsController < ApplicationController
       @trainer = Trainer.find(params[:trainer_id])
       @pokemons = @trainer.pokemons
     end
-
   end
-
 
   def new
     @trainer = Trainer.find(params[:id])
