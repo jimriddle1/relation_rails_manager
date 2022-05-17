b1bf39f8f7e2f6feb24bb5f07c74cdda2e6e0f9a
Jim Riddle <jimriddle0@gmail.com>
2022-05-16 20:23:37 -0500

    updated controller to include a where

--- spec/features/pokemons/index_spec.rb
+++ spec/features/pokemons/index_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'the pokemons index page' do
     # When I visit '/child_table_name'
     # Then I see each Child in the system including the Child's attributes:
     trainer = Trainer.create!(name: "Ash", age: 18, all_8_badges: false)
-    pokemon = trainer.pokemons.create!(name: "Squirtle", pokedex_num: 7, fainted: false)
-    pokemon_2 = trainer.pokemons.create!(name: "Charmander", pokedex_num: 4, fainted: false)
+    pokemon = trainer.pokemons.create!(name: "Squirtle", pokedex_num: 7, fainted: true)
+    pokemon_2 = trainer.pokemons.create!(name: "Charmander", pokedex_num: 4, fainted: true)
     # binding.pry
     visit "/pokemons"
     # save_and_open_page
@@ -35,14 +35,14 @@ RSpec.describe 'the pokemons index page' do
     expect(page).to have_link('Trainer Index')
   end
 
-  it 'only displays pokemon that are not fainted' do
+  it 'only displays pokemon that are fainted' do
     # As a visitor
     # When I visit the child index
     # Then I only see records where the boolean column is `true`
     trainer = Trainer.create!(name: "Ash", age: 18, all_8_badges: false)
-    pokemon = trainer.pokemons.create!(name: "Squirtle", pokedex_num: 7, fainted: false)
-    pokemon_2 = trainer.pokemons.create!(name: "Charmander", pokedex_num: 4, fainted: false)
-    pokemon_3 = trainer.pokemons.create!(name: "Bulbasaur", pokedex_num: 1, fainted: true)
+    pokemon = trainer.pokemons.create!(name: "Squirtle", pokedex_num: 7, fainted: true)
+    pokemon_2 = trainer.pokemons.create!(name: "Charmander", pokedex_num: 4, fainted: true)
+    pokemon_3 = trainer.pokemons.create!(name: "Bulbasaur", pokedex_num: 1, fainted: false)
 
     visit "/pokemons"
 
