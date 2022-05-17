cf4b50ece1223f948008153b831994a0bcca317e
Jim Riddle <jimriddle0@gmail.com>
2022-05-17 13:01:02 -0500

    wrote test for alphabetical pokemons

--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -12,15 +12,9 @@ RSpec.describe Trainer do
   end
 
   it 'displays the trainers in order by creation date desc order' do
-    # As a visitor
-    # When I visit the parent index,
-    # I see that records are ordered by most recently created first
-    # And next to each of the records I see when it was created
 
     trainer = Trainer.create!(name: "Ash", age: 18, all_8_badges: false)
     trainer_2 = Trainer.create!(name: "Brock", age: 21, all_8_badges: false)
-
-
     expect(Trainer.trainers_in_order).to eq([trainer_2, trainer])
   end
 end
